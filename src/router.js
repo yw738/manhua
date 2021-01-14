69e35b193fe8869599ec89912e8035125ba201fd
meibo <941055134@qq.com>
2021-01-14 15:51:45 +0800

    1、捕获router  push的异常

--- src/router.js
+++ src/router.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+};
 export default new Router({
     mode: 'hash',
     base: process.env.BASE_URL,
