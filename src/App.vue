9897c25ff970d6928fed461229db17bdbc16893b
yangwei <http://git.shuipingsiwei.cn/>
2021-08-16 17:16:33 +0800

    链接优化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    <!-- include 希望缓存的组件   -->
-    <!-- exclude 不希望被缓存的组件 -->
-    <!-- exclude 优先级高于 include -->
-    <keep-alive include="AboutIndex" max="1" :exclude="isKeepalive?'':'AboutIndex'">
-      <router-view v-if="isShow" />
-    </keep-alive>
+    <div v-if="isAllowShow">
+      <!-- include 希望缓存的组件   -->
+      <!-- exclude 不希望被缓存的组件 -->
+      <!-- exclude 优先级高于 include -->
+      <keep-alive
+        include="AboutIndex"
+        max="1"
+        :exclude="isKeepalive ? '' : 'AboutIndex'"
+      >
+        <router-view v-if="isShow" />
+      </keep-alive>
+    </div>
+    <div v-else>
+      <div>测试测试测试</div>
+      <van-skeleton title avatar :row="3" />
+    </div>
   </div>
 </template>
 <script>
@@ -13,14 +23,16 @@ import { Dialog } from "vant";
 export default {
   data() {
     return {
-      isShow: true,//验证用户是否是移动端预览
-      isKeepalive: false,//是否对详情进行缓存
+      isShow: true, //验证用户是否是移动端预览
+      isKeepalive: false, //是否对详情进行缓存
+
+      isAllowShow: false, //是否允许显示页面，初次进来会显示一次，仅从github进来的时候才会为true
     };
   },
   methods: {
     /**
      * 验证设备
-    */
+     */
     init() {
       var sUserAgent = navigator.userAgent.toLowerCase();
       var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
@@ -48,38 +60,53 @@ export default {
         this.isShow = false;
         Dialog.alert({
           title: "提示",
-          message: "请使用移动设备来浏览该网页,以此得到更友好的体验!"
+          message: "请使用移动设备来浏览该网页,以此得到更友好的体验!",
         }).then(() => {
           // on close
         });
       }
-    }
+    },
   },
   created() {
-    // this.init();
+    //验证来源，非 github 直接拦截
+    let sourceIsAllow =
+      location.href.split("?")[1] &&
+      location.href.split("?")[1].includes("github");
+    if (sourceIsAllow) {
+      this.isAllowShow = true;
+      window.sessionStorage.setItem("sourceIsAllow", true);
+    } else {
+      this.isAllowShow = false;
+    }
   },
   watch: {
-    //监听路由 判断是否进行keep-alive动态缓存 
-    $route: function(to, from) {
-      if (to.name === "detail"||to.name === "about") {
+    //监听路由 判断是否进行keep-alive动态缓存
+    $route: function (to, from) {
+      let source = window.sessionStorage.getItem("sourceIsAllow");
+      if (source == "true") {
+        this.isAllowShow = true;
+      } else {
+        this.isAllowShow = false;
+      }
+      if (to.name === "detail" || to.name === "about") {
         this.isKeepalive = true;
-      }else{
+      } else {
         this.isKeepalive = false;
       }
-    }
+    },
   },
 };
 </script>
 
 <style lang="less">
 html {
-    font-size: 100vw / 3.75;
+  font-size: 100vw / 3.75;
 }
 
 body {
-    font-size: 0.14rem;
+  font-size: 0.14rem;
 }
-#app{
+#app {
   overflow: hidden;
 }
 </style>
